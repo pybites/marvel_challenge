d5e31b6a33fc6b419d79190b8ea95dc26326ea0c
bbelderbos <bobbelderbos@gmail.com>
2017-11-07 10:41:18 +0100

    some more clarifications in docstrings

--- marvel.py
+++ marvel.py
@@ -8,10 +8,10 @@ Character = namedtuple('Character', 'pid name sid align sex appearances year')
 
 
 def convert_csv_to_dict(data=DATA):
-    '''write a function to parse marvel-wikia-data.csv into a dict
+    '''write a function to parse marvel-wikia-data.csv, see
        https://docs.python.org/3.7/library/csv.html#csv.DictReader
        should return a list of OrderedDicts or a list of Character
-       namedtuples (see 'Character' above')'''
+       namedtuples (see Character namedtuple above')'''
     pass
 
 
@@ -20,20 +20,22 @@ data = list(convert_csv_to_dict())
 
 def most_popular_characters(n=5):
     '''get the most popular character by number of appearances
-       accept an argument of n = number of most popular characters
-       to return'''
+       accept an argument of n (int) of most popular characters
+       to return (leave default of 5)'''
     pass
 
 
 def max_and_min_years_new_characters():
     '''Get the year with most and least new characters introduced respectively,
-       return a tuple of (max_year, min_year)'''
+       use either the 'FIRST APPEARANCE' or 'Year' column in the csv data, or
+       the 'year' attribute of the namedtuple, return a tuple of
+       (max_year, min_year)'''
     pass
 
 
 def percentage_female():
-    '''Get the percentage of female characters, only look at male and female,
-       ignore the rest, return a percentage rounded to 2 digits'''
+    '''Get the percentage of female characters, only look at male and female
+       for total, ignore the rest, return a percentage rounded to 2 digits'''
     pass
 
 
@@ -43,8 +45,8 @@ def good_vs_bad(sex):
        to only receive 'male' or 'female' as valid inputs (should
        be case insensitive, so could also pass it MALE)
 
-       The expected result should be a dict with these keys and
-       values percentages, rounded integers (values made up here):
+       The expected result should be a the following dict. The values are
+       rounded (int) percentages (values made up here):
 
        expected = {'Bad Characters': 33,
                    'Good Characters': 33,
